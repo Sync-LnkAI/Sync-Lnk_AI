72ed7eda4b1ef49973349ff0cd373f1e20dad28f
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-02 16:11:12 +0900

    chat2

--- app.py
+++ app.py
@@ -1097,6 +1097,7 @@ if is_admin:
 
                     async_thread = threading.Thread(target=background_async_tasks, args=(recent_messages,  "なし"))
                     async_thread.start()
+                    st.rerun()
 
     # ------------------------------------------------------------------
     # 🎨 【管理者・タブ2】 キャラクター・見た目設定画面
