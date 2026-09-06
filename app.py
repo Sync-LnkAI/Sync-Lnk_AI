8c587626510fc1715bf4ad54c9865004efb4819d
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-06 16:25:06 +0900

    text2

--- app.py
+++ app.py
@@ -1334,9 +1334,8 @@ st.markdown(f"""
         background-image: none !important;
         outline: none !important;
     }}
-    div[data-testid="stTextArea"] textarea:focus {{
+    div[data-testid="stTextArea"]:focus-within {{
         border-color: {theme_cfg["input_border"]} !important;
-        outline: none !important;
         box-shadow: 0 0 0 2px {theme_cfg["input_border"]} !important;
     }}
  
