a664f1303de33b3ee612bea47709208044264c23
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-06 20:53:09 +0900

    text6

--- app.py
+++ app.py
@@ -1334,7 +1334,11 @@ st.markdown(f"""
         background-image: none !important;
         outline: none !important;
     }}
-    .st-emotion-cache-1mnb0ez:focus-within {{
+    div[data-testid="stTextInput"] > div:focus-within {{
+        border-color: {{theme_cfg["input_border"]}} !important;
+    }}
+
+    div[data-testid="stTextInput"] > div:focus-within * {{
         border-color: {{theme_cfg["input_border"]}} !important;
     }}
  
