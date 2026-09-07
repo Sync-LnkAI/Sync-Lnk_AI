63436c2471db3a559e01b3dfd6c658e445207643
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-08 01:37:35 +0900

    error13

--- app.py
+++ app.py
@@ -1843,6 +1843,10 @@ with all_tabs[0]:
 # 🎨 【タブ2】 話し方・見た目設定
 # ------------------------------------------------------------------
 with all_tabs[1]:
+        print(
+            "読込テーブル名=",
+            DB_MEMORIES_TABLE
+        )
         check = (
             supabase
             .table(DB_MEMORIES_TABLE)
