658a9df6180621f5c8ad866b62a5e9d0d919c8a6
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-08 01:25:59 +0900

    error11

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ if not user_param:
 CURRENT_USER_ID = str(user_param)
 ADMIN_USER_ID = st.secrets["ADMIN_USER_ID"]
 
+st.write(CURRENT_USER_ID)
 # アプリのURLパラメーター（または headless 状態）を見て、自動学習の書き込み先を全自動で仕分けます
 #is_dev_site = "dev" in st.query_params or st.config.get_option("server.headless") == False
 #is_dev_site = "dev" in st.query_params.to_dict() or st.config.get_option("server.headless") == False
