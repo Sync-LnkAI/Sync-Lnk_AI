bcdbc0d74fc94155b614b78ff89c58967292e3ac
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-08 17:08:09 +0900

    settei1

--- app.py
+++ app.py
@@ -1902,6 +1902,8 @@ with all_tabs[0]:
 # 🎨 【タブ2】 話し方・見た目設定
 # ------------------------------------------------------------------
 with all_tabs[1]:
+        st.write(f"🔍 金庫からロードされた現在の変数の中身: 【 {current_style_preset} 】")
+
         st.write(f"### 🎨 {current_concierge_name}のカスタマイズ")
         st.caption("AIの話し方・見た目・アプリのデザインを自分の好みに設定できます。")
 
