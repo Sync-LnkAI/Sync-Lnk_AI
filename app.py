937339da1b341091d5bfacedd43ab6f539802ccb
ryuudesuyo <ryuudesuyo@yahoo.co.jp>
2026-09-09 23:34:43 +0900

    settei21

--- app.py
+++ app.py
@@ -2282,6 +2282,18 @@ with all_tabs[1]:
             #current_plan_idx = plan_options.index(st.session_state.current_user_plan_state) if st.session_state.current_user_plan_state in plan_options else 0
             #new_plan = st.selectbox("現在の会員プラン", plan_options, index=current_plan_idx)
 
+        st.divider()
+        #st.markdown("---")
+
+        st.markdown("🧠 現在AIが覚えていること")
+        if summary_memory_context != "なし":
+            #st.info(summary_memory_context)
+            st.markdown(summary_memory_context.replace("\n"," \n"))
+        else:
+            st.caption(
+                "まだ覚えている情報はありません。"
+            )
+
             if st.form_submit_button("基本設定を保存"):
                 with st.spinner("設定を登録しています...しばらくお待ちください"):
                     r1 = save_or_update_user_setting("AIの名前", new_concierge_name)
